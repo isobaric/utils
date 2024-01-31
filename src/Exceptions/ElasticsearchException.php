158a306ded254040d5224b1bcc87cd779b5a0787
yanhaochuang <yanhaochuang@qq.com>
2024-01-31 21:56:01 +0800

    修改命名空间

--- src/Exceptions/ElasticsearchException.php
+++ src/Exceptions/ElasticsearchException.php
@@ -1,8 +1,20 @@
 <?php
 
-namespace Horseloft\Utils\Exceptions;
+namespace Isobaric\Utils\Exceptions;
 
-class ElasticsearchException
+use Throwable;
+
+class ElasticsearchException extends \RuntimeException
 {
-
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     * @link https://php.net/manual/en/exception.construct.php
+     * @param string         $message  [optional] The Exception message to throw.
+     * @param int            $code     [optional] The Exception code.
+     * @param null|Throwable $previous [optional] The previous throwable used for the exception chaining.
+     */
+    public function __construct(string $message = "", int $code = ExceptionCode::ELASTICSEARCH_ERROR_CODE, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
 }
