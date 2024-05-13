309e249ebf090abe7bf5877d21fb8448524fba2d
yanhaochuang <yanhaochuang@qq.com>
2024-05-13 18:30:20 +0800

    移除mixed参数类型声明

--- src/NumericUtil.php
+++ src/NumericUtil.php
@@ -7,10 +7,10 @@ class NumericUtil
     /**
      * 是否为整数；注意：多个0时返回true，即 000 或 '000' 都是整数
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isInt(mixed $numeric): bool
+    public static function isInt($numeric): bool
     {
         if (is_float($numeric) || !is_numeric($numeric)) {
             return false;
@@ -22,10 +22,10 @@ class NumericUtil
     /**
      * 是否为自然数，即非负整数
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isNatInt(mixed $numeric): bool
+    public static function isNatInt($numeric): bool
     {
         return self::isInt($numeric) && $numeric >= 0;
     }
@@ -33,10 +33,10 @@ class NumericUtil
     /**
      * 是否为正整数
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isPosInt(mixed $numeric): bool
+    public static function isPosInt($numeric): bool
     {
         return self::isInt($numeric) && $numeric > 0;
     }
@@ -44,10 +44,10 @@ class NumericUtil
     /**
      * 是否为负整数
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isNegInt(mixed $numeric): bool
+    public static function isNegInt($numeric): bool
     {
         return self::isInt($numeric) && $numeric < 0;
     }
@@ -55,10 +55,10 @@ class NumericUtil
     /**
      * 是否为小数；注意：0.0000 == 0.0 或 '0.0000' == 0.0
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isDecimal(mixed $numeric): bool
+    public static function isDecimal($numeric): bool
     {
         if (!is_float($numeric) && !is_numeric($numeric)) {
             return false;
@@ -69,10 +69,10 @@ class NumericUtil
     /**
      * 是否为大于等于小数；>=0.0
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isNatDecimal(mixed $numeric): bool
+    public static function isNatDecimal($numeric): bool
     {
         return self::isDecimal($numeric) && $numeric >= 0;
     }
@@ -80,10 +80,10 @@ class NumericUtil
     /**
      * 是否为大于0的小数；>=0.0...1
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isPosDecimal(mixed $numeric): bool
+    public static function isPosDecimal($numeric): bool
     {
         return self::isDecimal($numeric) && $numeric > 0;
     }
@@ -91,10 +91,10 @@ class NumericUtil
     /**
      * 是否为小于0的小数；< 0.0
      *
-     * @param mixed $numeric
+     * @param $numeric
      * @return bool
      */
-    public static function isNegDecimal(mixed $numeric): bool
+    public static function isNegDecimal($numeric): bool
     {
         return self::isDecimal($numeric) && $numeric < 0;
     }
@@ -103,12 +103,12 @@ class NumericUtil
      * 是否为限定小数位的小数
      *  不支持float类型零的长度限制：0.0 / 0.000 等；此时限制长度无效
      *  应使用string类型的零限制长度：'0.00' / '0.000' 等
-     * @param mixed $numeric
+     * @param $numeric
      * @param int $min
      * @param int|null $max
      * @return bool
      */
-    public static function isLimitDecimal(mixed $numeric, int $min, int $max = null): bool
+    public static function isLimitDecimal($numeric, int $min, int $max = null): bool
     {
         if (is_null($max)) {
             $limit = '{' . $min . '}';
