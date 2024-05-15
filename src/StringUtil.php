e5ec8183b85175fae8bf35c33d39763f99e020dc
yanhaochuang <yanhaochuang@qq.com>
2024-05-15 19:33:30 +0800

    新增驼峰字符串转下划线字符串

--- src/StringUtil.php
+++ src/StringUtil.php
@@ -13,4 +13,20 @@ class StringUtil
     {
         return filter_var($email, FILTER_VALIDATE_EMAIL) !== false;
     }
+
+    /**
+     * 将驼峰（大驼峰或小驼峰）格式的字符串转换为下划线格式
+     *
+     * @param string|null $camelCaseStr 驼峰格式的字符串（大驼峰或小驼峰）
+     * @return string 下划线格式的字符串
+     */
+    public static function camelToUnderline(?string $camelCaseStr): string
+    {
+        if (strlen($camelCaseStr) == 0) {
+            return '';
+        }
+
+        $underlineStr = preg_replace('/(?<=\\w)(?=[A-Z])/', '_$1', $camelCaseStr);
+        return strtolower($underlineStr);
+    }
 }
