01d949e6012f1031155ad58e2eb074dded224881
yanhaochuang <yanhaochuang@qq.com>
2024-10-21 20:03:29 +0800

    新增连接异常

---
+++ src/Exceptions/ConnectionException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Isobaric\Utils\Exceptions;
+
+use Throwable;
+
+class ConnectionException extends \RuntimeException
+{
+    /**
+     * Construct the exception. Note: The message is NOT binary safe.
+     * @link https://php.net/manual/en/exception.construct.php
+     * @param string         $message  [optional] The Exception message to throw.
+     * @param int            $code     [optional] The Exception code.
+     * @param null|Throwable $previous [optional] The previous throwable used for the exception chaining.
+     */
+    public function __construct(string $message = '', int $code = ExceptionCode::CONNECTION_ERROR_CODE, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+}
